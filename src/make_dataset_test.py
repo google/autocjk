329f90c1e85d69a0bfb961536600abafdea4b540
James Buckland <jbuckland@google.com>
2021-03-14 19:41:46 -0400

    Use perceptual diff instead.

--- src/make_dataset_test.py
+++ src/make_dataset_test.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 from PIL import Image
-from PIL import ImageChops
 from tensorflow.python.platform import googletest
+import imagehash
 import os
 import subprocess
 import tempfile
@@ -25,13 +25,17 @@ import tempfile
 class TestMakeDataset(googletest.TestCase):
     def test_make_dataset(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
-            subprocess.run(["src/make_dataset", "--dst", tmp_dir, "--fonts",
-                            "src/testutils/NotoSerifCJKsc-Regular.otf",
-                            "--limit=1", "--num_workers=1"])
+            subprocess.run([
+                "src/make_dataset", "--dst", tmp_dir, "--fonts",
+                "src/testutils/NotoSerifCJKsc-Regular.otf", "--limit=1",
+                "--num_workers=1"
+            ])
             im_actual = Image.open(os.path.join(tmp_dir, "0-0013334.png"))
             im_expected = Image.open("src/testutils/0-0013334.png")
-            self.assertEqual(ImageChops.difference(
-                im_actual, im_expected).getbbox(), None)
+            self.assertEqual(
+                0,
+                imagehash.average_hash(im_actual) -
+                imagehash.average_hash(im_expected))
 
 
 if __name__ == "__main__":
