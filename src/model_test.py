a3a1a1b5b93a978c2dbd8ca67091e3743b81e23f
James Adam Buckland <jbuckland@google.com>
2021-09-26 10:20:25 -0400

    Add model_test to assert generator correctness. (#28)
    We just run on a limited corpus for one epoch, but it allows us to assert input/output dimensions, run the whole training pipeline in a unit test, and ensure there are no runtime tensorflow issues with the resultant generator.

--- src/model_test.py
+++ src/model_test.py
@@ -15,9 +15,12 @@
 from PIL import Image
 from tensorflow.python.platform import googletest
 import tempfile
+import tensorflow as tf
 
 from src import model as model_lib
+from src import imageutils
 
+_PATH_TO_FONT = "src/testutils/NotoSerifCJKsc-Regular.otf"
 _PATH_TO_TRAINING_IMAGE = "src/testutils/training_data/0-0013312.png"
 _PATH_TO_TRAINING_IMAGE_WRONG_SIZE = "src/testutils/training_data/0-0013312-250.png"
 
@@ -45,6 +48,33 @@ class TestClass(googletest.TestCase):
         self.assertEqual(a.shape, (256, 256, 1))
         self.assertEqual(b.shape, (256, 256, 1))
         self.assertEqual(a_and_b.shape, (256, 256, 1))
+
+
+class TrainTestClass(googletest.TestCase):
+    def test_train_one_epoch(self):
+        train_dataset, test_dataset = model_lib.make_datasets(
+            'src/testutils/corpus/0-*.png')
+
+        generator = model_lib.make_generator()
+        discriminator = model_lib.make_discriminator()
+        loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+        discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+        epochs = 1
+        model_lib.fit(generator, generator_optimizer, discriminator,
+                      discriminator_optimizer, loss_object, train_dataset,
+                      epochs, test_dataset)
+
+        self.assertEqual(generator.input_shape, (None, 256, 256, 2))
+        self.assertEqual(generator.output_shape, (None, 256, 256, 1))
+        self.assertFalse(generator.run_eagerly)
+
+        with tempfile.NamedTemporaryFile(mode='w+', suffix=".png") as tmp_file:
+            # We can't assert anything about the generated image, but we just
+            # want to see that imageutils.predict(generator, ...) doesn't fail.
+            imageutils.predict(generator, _PATH_TO_FONT, '市', '來',
+                               tmp_file.name)
 
 
 if __name__ == "__main__":
